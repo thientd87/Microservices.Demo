f4d437efd6110881cf0349a4b26466d37662d367
thientd87 <thientd87@gmail.com>
2020-11-05 21:01:39 +0700

    Add Web project

---
+++ Microservices.Demo.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Microservices.Demo.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
