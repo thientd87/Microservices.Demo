073035f16eeb0e7cb37b1ae3552c5bd23bc1a970
thientd87 <thientd87@gmail.com>
2020-11-09 21:30:18 +0700

    add entities and controller

---
+++ src/Catalog/Catalog.API/Entities/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.API.Entities
+{
+    public class Product
+    {
+        public Product()
+        {
+        }
+
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+        [BsonElement("Name")]
+        public string Name { get; set; }
+
+        [BsonElement("Summary")]
+        public string Summary { get; set; }
+
+        [BsonElement("Description")]
+        public string Description { get; set; }
+
+        [BsonElement("ImageFile")]
+        public string ImageFile { get; set; }
+
+        [BsonElement("Category")]
+        public string Category { get; set; }
+
+        [BsonElement("Price")]
+        public decimal Price { get; set; }
+
+
+    }
+}
